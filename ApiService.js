09998c36b66a399371929c0a0b32e46fd5abe2b5
boxeggg <kamilszydlowski53@gmail.com>
2024-06-12 15:16:38 +0200

    new methods in api service

--- ApiService.js
+++ ApiService.js
@@ -1,11 +1,68 @@
 import axios from "axios";
-class ApiService{
+class ApiService {
+    constructor(url) {
+        this.url = url
+    }
+    async getLastRequest() {
+        try {
+            const response = await axios.get(`http://${this.url}/api/Request/last`);
+            return response.data;
+        } catch (error) {
+            if (error.response) {
+                return {
+                    status: false,
+                    message: error.response.data || error.message
+                };
+            } else if (error.request) {
 
-async getLastRequest () {
-    const response =  await axios.get("http://localhost:5205/api/Request/last");
-    return response.data.user;
-}
+                return {
+                    status: false,
+                    message: 'No response received from server'
+                };
+            } else {
+                return {
+                    status: false,
+                    message: error.message
+                };
+            }
+        }
+    }
 
-    
+    async getAllRequest() {
+        const response = await axios.get(`http://${this.url}/api/Request/all`);
+        return response.data;
+    }
+    async deleteLastRequest() {
+        try {
+            const response = await axios.delete(`http://${this.url}/api/Request/last`);
+            return response.data;
+        } catch (error) {
+            if (error.response) {
+                return {
+                    status: false,
+                    message: error.response.data || error.message
+                };
+            } else if (error.request) {
+
+                return {
+                    status: false,
+                    message: 'No response received from server'
+                };
+            } else {
+                return {
+                    status: false,
+                    message: error.message
+                };
+            }
+
+
+        }
+
+    }
+    async getRequestCount() {
+        const response = await axios.get(`http://${this.url}/api/Request/count`);
+        return response.data;
+    }
+
 }
 export default ApiService;
--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import ApiService from "./ApiService.js";
-let apiService = new ApiService();
-console.log(await apiService.getLastRequest());
-
-
-
-
-
+let apiService = new ApiService("localhost:5205");
+while(await apiService.getRequestCount() > 0){
+    console.log(await apiService.getLastRequest())
+    await apiService.deleteLastRequest();
+}
