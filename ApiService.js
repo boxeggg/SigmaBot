412cd29d206fcf92d741f36f8074faed5379e0bf
boxeggg <kamilszydlowski53@gmail.com>
2024-06-13 17:41:01 +0200

    supported post and patch api requests

--- ApiService.js
+++ ApiService.js
@@ -63,6 +63,38 @@ class ApiService {
         const response = await axios.get(`http://${this.url}/api/Request/count`);
         return response.data;
     }
+    async getStatus() {
+        const response = await axios.get(`http://${this.url}/api/Status`);
+        return response.data;
+    }
+    async updateStatus(status){
+        const response = await axios.patch(`http://${this.url}/api/Status`, status);
+        return response.data;
+    }
+    async addRequest(request){
+        try{
+        const response = await axios.post(`http://${this.url}/api/Request/new`, request);
+        return response.data;
+        } catch (error) {
+            if (error.response) {
+                return {
+                    status: false,
+                    message: error.response.data || error.message
+                };
+            } else if (error.request) {
+
+                return {
+                    status: false,
+                    message: 'No response received from server'
+                };
+            } else {
+                return {
+                    status: false,
+                    message: error.message
+                };
+            }
+        }
+    }
 
 }
 export default ApiService;
--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import ApiService from "./ApiService.js";
 let apiService = new ApiService("localhost:5205");
-while(await apiService.getRequestCount() > 0){
-    console.log(await apiService.getLastRequest())
-    await apiService.deleteLastRequest();
-}
+
+await apiService.getLastRequest()
+console.log(status)
